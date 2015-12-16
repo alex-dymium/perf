0ad1822be4df2b6ee918688c0d4d6c520de4579b
rusual <alexandru.rusu@one.verizon.com>
2015-12-16 15:26:37 -0800

    1.3.0.94789

--- test_framework/draft.py
+++ test_framework/draft.py
@@ -12,6 +12,7 @@ def run(ip):
 
 
 if __name__ == "__main__":
+    create_report(0,'hub2')
     # all_devices_ip(run)
     # finish(0)
-    all_devices_ip(delit_last_report)
+    # all_devices_ip(delit_last_report)
